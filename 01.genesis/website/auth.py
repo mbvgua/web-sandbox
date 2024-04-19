b5562690a6a90df4f3503a034385036fd9d9e44d
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-04-19 05:10:37 +0300

    01.genesis -> update .gitignore file to ignore image files

--- 01.genesis/website/auth.py
+++ 01.genesis/website/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint,render_template,redirect, url_for,request
 from .forms import SignUpForm, LoginForm
+from .models import User
 
 
 auth = Blueprint('auth', __name__)
@@ -9,6 +10,10 @@ auth = Blueprint('auth', __name__)
 def signup():
     # instantiate the form
     form = SignUpForm()
+    user = User.query.filter_by(email=email).first()
+    # if request.method == 'POST':
+    #     if form.validate():
+
 
     return render_template('signup.html', form=form)
 
